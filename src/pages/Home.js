9b0cbb3d206e7e94dbd3157fa51ef7da49a44c2b
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2024-10-03 15:05:29 +0530

    Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,35 @@
+// src/pages/Home.js
+
 import React from 'react';
-import './Home.css';
+import './Home.css'; // Create this CSS file for styling
+// Import icons for social media and email
+import { FaFacebook, FaEnvelope } from 'react-icons/fa'; 
 
 const Home = () => {
   return (
-    <section id="home" className="home-section">
-      <h1>Welcome to My Portfolio</h1>
-      <p>I am a web developer skilled in React.js and Node.js.</p>
-    </section>
+    <div className="home-container">
+      <div className="home-content">
+        <h1>Hello, I'm</h1>
+        <h2>Deepika S Rajapaksha</h2> {/* Name in a separate line */}
+        <p>I am a passionate Web Developer and UI/UX Designer dedicated to creating beautiful and functional websites. I love turning ideas into reality with clean and efficient code.</p> {/* Your description */}
+
+        {/* Social media and contact links */}
+        <div className="contact-options">
+          <a href="https://www.facebook.com/yourprofile" target="_blank" rel="noopener noreferrer">
+            <FaFacebook className="contact-icon" />
+          </a>
+          <a href="mailto:deepika@example.com">
+            <FaEnvelope className="contact-icon" />
+          </a>
+        </div>
+      </div>
+      <div className="home-image">
+      <img src="/images/DP.jpg" alt="Deepika Rajapaksha" /> {/* Reference to your image */}
+      </div>
+    </div>
   );
 };
 
 export default Home;
+
 
